12ef256f2dbcb4c40e3892a289d6a6473e716905
João Vitor <joaovitorpr0play@gmail.com>
2020-02-14 08:26:30 -0300

    arquivo de teste

--- teste.c
+++ teste.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
 
+int main()
+{
+    int a;
+printf("*Idade do usuario");
+scanf("%d", &a);
+a-1;
+printf("Idade do usuario",&a);
 
     return 0;
 }
